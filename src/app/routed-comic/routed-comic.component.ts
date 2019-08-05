96e7cb8640183d089ae6dc0d646acc779ad6b35a
Manfred Steyer <manfred.steyer@gmx.net>
2019-08-05 23:49:36 +0200

    bind to props

--- src/app/routed-comic/routed-comic.component.ts
+++ src/app/routed-comic/routed-comic.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit, ViewChild, AfterContentInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-routed-comic',
   templateUrl: './routed-comic.component.html',
   styleUrls: ['./routed-comic.component.css']
 })
-export class RoutedComicComponent implements OnInit, AfterViewInit {
+export class RoutedComicComponent implements OnInit
+// , AfterViewInit 
+{
 
+  constructor(private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  @ViewChild('wrapped', { static: true })
-  component: any;
+  // @ViewChild('wrapped', { static: true })
+  // component: any;
+  params: any = {};
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.params = params;
+    });
   }
 
-  ngAfterViewInit(): void {
-    console.debug('keys', this.component.constructor.ngComponentDef);
-  }
+  // ngAfterViewInit(): void {
+  //   console.debug('keys', this.component.constructor.ngComponentDef);
+  // }
 }
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,125 +1,63 @@
 import {
-    ɵcompileComponent,
     ɵɵdefineComponent,
     ɵɵdirectiveInject,
-    ɵrenderComponent,
     ɵComponentDef,
-    ɵComponentType,
-    ɵmarkDirty,
-    Type,
-    Host,
-    ElementRef,
-    Injector,
     ChangeDetectionStrategy,
-    ContentChild,
     ɵɵelement,
-    ɵɵviewQuery,
-    ɵɵstaticViewQuery,
-    ɵɵqueryRefresh,
-    ɵɵloadViewQuery,
-    AfterContentInit,
     OnInit,
-    AfterViewInit,
-    ChangeDetectorRef
+    ɵɵproperty
 } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
-
+import { RenderFlags } from '@angular/compiler/src/core';
 
 export function withRoute(component /*: ɵComponentType<any>*/) {
 
     const def = component.ngComponentDef as ɵComponentDef<any>;
-
     const elementName = def.selectors[0][0] as string;
 
     // TODO: using ɵComponentType leads to an error, b/c the type
     //  does not have ngComponentDef at compile time
-    class HigherOrderComponent implements AfterViewInit {
+    class HigherOrderComponent implements OnInit {
 
         static ngComponentDef: ɵComponentDef<HigherOrderComponent>;
 
-        component: any;
+        params: any = {};
 
-        ngAfterViewInit(): void {
-           this.setupParams();
-           this.cd.detectChanges();
+        constructor(private route: ActivatedRoute) {
         }
 
-        setupParams() {
-            const comp = this.component;
-
+        ngOnInit() {
             this.route.params.subscribe(params => {
-                console.debug('p', params);
-                
-                for (const propName in def.inputs) {
-                    if (params[propName]) {
-                        console.debug('new value', params[propName]);
-                        comp[propName] = params[propName];
-                        console.debug('!', propName);
-                        // ɵmarkDirty(comp);
-                    }
-                }
-
+              this.params = params;
             });
         }
-
-        constructor(
-            private cd: ChangeDetectorRef,
-            private injector: Injector,
-            private host: ElementRef,
-            private route: ActivatedRoute) {
-                
-                // const comp = ɵrenderComponent(component, { host: this.host.nativeElement, injector });
-                // const compType = comp.constructor as ɵComponentType<any>;
-               
-                
-                
-                
-
-
-        }
-    };
-
-    
-
+    }
 
     HigherOrderComponent.ngComponentDef = ɵɵdefineComponent({
-        consts: 20,
-        vars: 20,
+        consts: 1,
+        vars: 1,
         directives: [
             component
         ],
         changeDetection: ChangeDetectionStrategy.Default,
-        factory: (f) => new HigherOrderComponent(
-            ɵɵdirectiveInject(ChangeDetectorRef as any),
-            ɵɵdirectiveInject(Injector as any),
-            ɵɵdirectiveInject(ElementRef),
+        factory: () => new HigherOrderComponent(
             ɵɵdirectiveInject(ActivatedRoute)),
-        // factory: (f) => new (f || HigherOrderComponent)(ɵɵdirectiveInject(ActivatedRoute)),
         selectors: [[]],
-        viewQuery: (rf, ctx) => {
-            if (rf & 1) {
-                ɵɵstaticViewQuery(['wrapped'], true, null);
+        template: (rf, ctx) => {
+            if (rf & RenderFlags.Create) {
+               ɵɵelement(0, elementName, null, ['wrapped', '']);
             }
-            if (rf & 2) {
-                let value;
-                if (ɵɵqueryRefresh((value = ɵɵloadViewQuery()))) {
-                    ctx.component = value.first;
+            if (rf & RenderFlags.Update) {
+                for (const prop in ctx.params) {
+                    if (prop in component) {
+                       ɵɵproperty(prop, ctx.params[prop]);
+                    }
                 }
             }
         },
-        template: (rf, ctx) => { 
-            
-            if (rf & 1) {
-                console.debug(elementName);
-               ɵɵelement(0, elementName, null, ['wrapped', '']);
-            }
-        },
-        // tslint:disable-next-line: whitespace
         type: HigherOrderComponent,
     });
 
     return HigherOrderComponent;
-
 }
